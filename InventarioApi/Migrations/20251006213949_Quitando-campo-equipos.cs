e6b129a528028a199eed50625e2d6edaab85da9b
jesusdepazz <rodolfomoralesdepaz03@gmail.com>
2025-10-06 17:39:50 -0600

    Agregando cambios para carga masiva

---
+++ InventarioApi/Migrations/20251006213949_Quitando-campo-equipos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventarioApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Quitandocampoequipos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistroDeprec",
+                table: "Equipos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RegistroDeprec",
+                table: "Equipos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- InventarioApi/Migrations/20251006231401_Quitando-campos-excel.Designer.cs
+++ InventarioApi/Migrations/20251006231401_Quitando-campos-excel.Designer.cs
@@ -4,6 +4,7 @@ using Inventory.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventarioApi.Migrations
 {
     [DbContext(typeof(InventarioContext))]
-    partial class InventarioContextModelSnapshot : ModelSnapshot
+    [Migration("20251006231401_Quitando-campos-excel")]
+    partial class Quitandocamposexcel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -483,6 +486,9 @@ namespace InventarioApi.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Comentarios")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Estado")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Extension")
