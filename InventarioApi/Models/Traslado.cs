66d7b5ebe01c1e049266423f2566f96ac0329a4f
jesusdepazz <rodolfomoralesdepaz03@gmail.com>
2025-12-17 17:55:21 -0600

    Agregando cambios para funcionalidad de formatos pdf

--- InventarioApi/Models/Traslado.cs
+++ InventarioApi/Models/Traslado.cs
@@ -5,8 +5,14 @@ public class Traslado
     public int Id { get; set; }
     public string No {  get; set; }
     public DateTime FechaEmision { get; set; }
-    public string PersonaEntrega { get; set; }
-    public string PersonaRecibe { get; set; }
+    public string CodigoEntrega { get; set; }
+    public string NombreEntrega { get; set; }
+    public string PuestoEntrega { get; set; }
+    public string DepartamentoEntrega { get; set; }
+    public string CodigoRecibe { get; set; }
+    public string NombreRecibe { get; set; }
+    public string PuestoRecibe { get; set; }
+    public string DepartamentoRecibe { get; set; }
     public string Equipo { get; set; }
     public string DescripcionEquipo { get; set; }
     public string Marca { get; set; }
