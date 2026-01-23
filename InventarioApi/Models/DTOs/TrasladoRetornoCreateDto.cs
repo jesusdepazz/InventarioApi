ed070fcb0ebbc0711399c9d215497299d576c3f2
jesusdepazz <rodolfomoralesdepaz03@gmail.com>
2026-01-23 12:16:20 -0600

    Agregando cambios para agregar mas equipos a formato traslados

---
+++ InventarioApi/Models/DTOs/TrasladoRetornoCreateDto.cs
@@ -0,0 +1,12 @@
+﻿public class TrasladoRetornoCreateDto
+{
+    public string No { get; set; }
+    public DateTime FechaPase { get; set; }
+    public string Solicitante { get; set; }
+    public string MotivoSalida { get; set; }
+    public DateTime? FechaRetorno { get; set; }
+    public string Status { get; set; }
+    public string RazonNoLiquidada { get; set; }
+
+    public List<TrasladoRetornoDetalleDto> Equipos { get; set; }
+}
---
+++ InventarioApi/Models/TrasladosRetornoDetalle.cs
@@ -0,0 +1,14 @@
+﻿public class TrasladoRetornoDetalle
+{
+    public int Id { get; set; }
+    public int TrasladoRetornoId { get; set; }
+
+    public string Equipo { get; set; }
+    public string DescripcionEquipo { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public string Serie { get; set; }
+    public string UbicacionRetorno { get; set; }
+
+    public TrasladoRetorno TrasladoRetorno { get; set; }
+}
